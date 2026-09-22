2f1b56aded3fcd4ed9538ff14ebde2343b96caaf
Loyd-14 <loyisosonti14@gmail.com>
2026-09-22 23:46:23 +0200

    Add files via upload

---
+++ LOYISO SONTI 0107145922087/DataValidationSys/src/datavalidationsys/DataValidationSys.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datavalidationsys;
+
+/**
+ *
+ * @author loyis
+ */
+public class DataValidationSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
